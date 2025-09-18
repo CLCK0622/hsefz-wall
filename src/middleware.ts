a2aacd34d55106c29e8938e3458a1daef0a37df2
CLCK0622 <zhongyi070622@163.com>
2025-09-18 01:46:10 -0500

    feat: add /user to public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ import { NextResponse } from 'next/server';
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/api/webhooks/clerk'
+    '/api/webhooks/clerk',
+    '/user'
 ]);
 
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
